72c70ba08f58296f81716d44a8ecbb824e91059d
Aoyu Zhang <aoyuzhan@amazon.com>
2020-12-14 21:05:20 +0800

    modify docker file for graph train

--- docker/graph/train/build_and_push.sh
+++ docker/graph/train/build_and_push.sh
@@ -12,7 +12,7 @@ account=$(aws sts get-caller-identity --query Account --output text)
 
 # Get the region defined in the current configuration (default to us-west-2 if none defined)
 region=$(aws configure get region)
-region=${region:-us-west-1}
+# region=${region:-us-west-1}
 
 fullname="${account}.dkr.ecr.${region}.amazonaws.com/${algorithm_name}:latest"
 
@@ -25,13 +25,20 @@ then
     aws ecr create-repository --repository-name "${algorithm_name}" > /dev/null
 fi
 
-# Get the login command from ECR and execute it directly
-$(aws ecr get-login --region ${region} --no-include-email)
+if [[ $region =~ ^cn.* ]]
+then
+    registry_id="727897471807"
+    registry_uri="${registry_id}.dkr.ecr.${region}.amazonaws.com.cn"
+else
+    registry_id="763104351884"
+    registry_uri="${registry_id}.dkr.ecr.${region}.amazonaws.com"
+fi
 
-# Build the docker image locally with the image name and then push it to ECR
-# with the full name.
+aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${registry_uri}
 
-docker build  -t ${algorithm_name} .
+docker build  -t ${algorithm_name} . --build-arg REGISTRY_URI=${registry_uri}
 docker tag ${algorithm_name} ${fullname}
+
+aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${account}.dkr.ecr.${region}.amazonaws.com.cn
 
 docker push ${fullname}
