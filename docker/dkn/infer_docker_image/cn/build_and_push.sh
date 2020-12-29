7e603f955be1cafecd38241a128ff8983a479c24
Aoyu Zhang <aoyuzhan@amazon.com>
2020-12-29 18:12:54 +0800

    all the build_and_push.sh for cn/global

--- docker/dkn/infer_docker_image/cn/build_and_push.sh
+++ docker/dkn/infer_docker_image/cn/build_and_push.sh
@@ -12,23 +12,30 @@ if [[ $region =~ ^cn.* ]]
 then
     registry_id="727897471807"
     registry_uri="${registry_id}.dkr.ecr.${region}.amazonaws.com.cn"
+    account_uri="${account_id}.dkr.ecr.${region}.amazonaws.com.cn"
 else
     registry_id="763104351884"
     registry_uri="${registry_id}.dkr.ecr.${region}.amazonaws.com"
+    account_uri="${account_id}.dkr.ecr.${region}.amazonaws.com"
 fi
 
 # login 到基础镜像的ecr
 aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${registry_uri}
 
+cd container
+# chmod +x kggraph/train
+chmod +x kggraph/serve
+
 # build image
 docker build -t $repo_name . --build-arg REGISTRY_URI=${registry_uri}
 
 # 打tag
-docker tag $repo_name $account_id.dkr.ecr.$region.amazonaws.com.cn/$repo_name:${tag}
+docker tag $repo_name $account_uri/$repo_name:${tag}
 
 # login 到自己的ecr
-aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${account_id}.dkr.ecr.${region}.amazonaws.com.cn
+aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${account_uri}
 # 判断自己的账户下有没有相应的repo
 aws ecr describe-repositories --repository-names $repo_name || aws ecr create-repository --repository-name $repo_name
 
-docker push $account_id.dkr.ecr.$region.amazonaws.com.cn/$repo_name:${tag}
+# push repo
+docker push $account_uri/$repo_name:${tag}
